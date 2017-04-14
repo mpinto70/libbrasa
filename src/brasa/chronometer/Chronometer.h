90c0bd18032456460760fa6252a43e720b1d00db
Marcelo Pinto <mpinto70@gmail.com>
2017-04-14 13:24:25 -0400

    Chronometer is 'complete'

--- src/brasa/chronometer/Chronometer.h
+++ src/brasa/chronometer/Chronometer.h
@@ -1,16 +1,42 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
 
 namespace chronometer {
 
+struct Elapsed {
+    uint32_t chrono_id;
+    uint32_t mark_id;
+    uint64_t begin;
+    uint64_t end;
+} __attribute__((packed));
+
+template<typename NOW_FUNC>
 class Chronometer {
 public:
-    Chronometer(uint32_t id)
-        : id_(id) {
+    Chronometer(NOW_FUNC&& func, uint32_t id)
+        : func_(std::move(func)),
+          id_(id),
+          begin_(func_()) {
     }
+    Chronometer(Chronometer&&) = default;
+    Chronometer& operator =(Chronometer&&) = default;
     uint32_t id() const { return id_; }
+    Elapsed mark(uint32_t mark_id) const {
+        return { id_, mark_id, begin_, func_() };
+    }
 private:
+    const NOW_FUNC func_;
     const uint32_t id_;
+    uint64_t begin_;
+
+    Chronometer(const Chronometer&) = delete;
+    Chronometer& operator =(const Chronometer&) = delete;
 };
+
+template<typename NOW_FUNC>
+Chronometer<NOW_FUNC> make_chronometer(NOW_FUNC&& func, uint32_t id) {
+    return Chronometer<NOW_FUNC>(std::forward<NOW_FUNC>(func), id);
+}
 }
--- test/brasa/chronometer/ChronometerTest.cpp
+++ test/brasa/chronometer/ChronometerTest.cpp
@@ -3,11 +3,38 @@
 
 #include <gtest/gtest.h>
 
+#include <chrono>
+#include <ctime>
+
 namespace chronometer {
+namespace {
+uint64_t Now() {
+    return std::chrono::high_resolution_clock::now().time_since_epoch().count();
+}
+constexpr timespec SLEEP = {0, 50000}; ///< 50 microseconds
+}
 
 TEST(ChronometerTest, create) {
-    const Chronometer chron(1);
-    EXPECT_EQ(chron.id(), 1u);
+    const auto chron1 = make_chronometer(Now, 1);
+    EXPECT_EQ(chron1.id(), 1u);
+    const auto chron2 = make_chronometer(Now, 7865);
+    EXPECT_EQ(chron2.id(), 7865u);
+}
+
+TEST(ChronometerTest, mark) {
+    const auto chron = make_chronometer(Now, 1234);
+    EXPECT_EQ(::nanosleep(&SLEEP, nullptr), 0);
+    const auto t1 = chron.mark(4321);
+    EXPECT_EQ(::nanosleep(&SLEEP, nullptr), 0);
+    const auto t2 = chron.mark(32);
+
+    EXPECT_EQ(t1.chrono_id, 1234u);
+    EXPECT_EQ(t1.mark_id, 4321u);
+    EXPECT_EQ(t2.chrono_id, 1234u);
+    EXPECT_EQ(t2.mark_id, 32u);
+
+    EXPECT_EQ(t1.begin, t2.begin);
+    EXPECT_LT(t1.end, t2.end);
 }
 
 }
