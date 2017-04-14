fab4062d152d2cf32672bf0bcbd7f736f89e83a2
Marcelo Pinto <mpinto70@gmail.com>
2017-04-14 13:29:52 -0400

    add functor test

--- test/brasa/chronometer/ChronometerTest.cpp
+++ test/brasa/chronometer/ChronometerTest.cpp
@@ -11,6 +11,19 @@ namespace {
 uint64_t Now() {
     return std::chrono::high_resolution_clock::now().time_since_epoch().count();
 }
+
+class NowFunctor {
+public:
+    NowFunctor(uint64_t initial)
+        : initial_(initial) {
+    }
+    uint64_t operator()() const {
+        return initial_++;
+    }
+private:
+    mutable uint64_t initial_;
+};
+
 constexpr timespec SLEEP = {0, 50000}; ///< 50 microseconds
 }
 
@@ -35,6 +48,22 @@ TEST(ChronometerTest, mark) {
 
     EXPECT_EQ(t1.begin, t2.begin);
     EXPECT_LT(t1.end, t2.end);
+}
+
+TEST(ChronometerTest, markWithFunctor) {
+    const auto chron = make_chronometer(NowFunctor(48), 1234);
+    const auto t1 = chron.mark(4321);
+    const auto t2 = chron.mark(32);
+
+    EXPECT_EQ(t1.chrono_id, 1234u);
+    EXPECT_EQ(t1.mark_id, 4321u);
+    EXPECT_EQ(t2.chrono_id, 1234u);
+    EXPECT_EQ(t2.mark_id, 32u);
+
+    EXPECT_EQ(t1.begin, t2.begin);
+    EXPECT_EQ(t1.begin, 48u);
+    EXPECT_EQ(t1.end, 49u);
+    EXPECT_EQ(t2.end, 50u);
 }
 
 }
