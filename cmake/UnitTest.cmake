b465918a7bb0f3e2961aca6815ba079038659515
Marcelo Pinto <mpinto70@gmail.com>
2018-11-26 11:17:59 -0500

    change the way to import gtest

--- cmake/UnitTest.cmake
+++ cmake/UnitTest.cmake
@@ -2,45 +2,14 @@
 # Google Test Setup - BEGIN ####################################################
 ################################################################################
 
-# We need thread support
-find_package(Threads REQUIRED)
-
-# Enable ExternalProject CMake module
-include(ExternalProject)
-
-# Download and install GoogleTest
-ExternalProject_Add(
-    gtest
-    URL https://github.com/google/googletest/archive/release-1.8.1.zip
-    PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/gtest
-    # Disable install step
-    INSTALL_COMMAND ""
+download_project(
+    PROJ                googletest
+    GIT_REPOSITORY      https://github.com/google/googletest.git
+    GIT_TAG             release-1.8.1
+    ${UPDATE_DISCONNECTED_IF_AVAILABLE}
 )
 
-# Get GTest source and binary directories from CMake project
-ExternalProject_Get_Property(gtest source_dir binary_dir)
-
-# Create a libgtest target to be used as a dependency by test programs
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest gtest)
-
-# Set libgtest properties
-set_target_properties(
-    libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/googlemock/gtest/libgtest.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
-
-# Create a libgmock target to be used as a dependency by test programs
-add_library(libgmock IMPORTED STATIC GLOBAL)
-add_dependencies(libgmock gtest)
-
-# Set libgmock properties
-set_target_properties(
-    libgmock PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/googlemock/libgmock.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
+add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
 
 ################################################################################
 # Google Test Setup - END ######################################################
@@ -61,14 +30,6 @@ function(add_unit_test test_name sources_var libs_var)
         ${${sources_var}}
     )
 
-    add_dependencies(${unit_test_name} libgtest)
-    add_dependencies(${unit_test_name} libgmock)
-
-    include_directories(
-        "${PROJECT_SOURCE_DIR}/gtest/src/gtest/googletest/include"
-        "${PROJECT_SOURCE_DIR}/gtest/src/gtest/googlemock/include"
-    )
-
     set_target_properties(
         ${unit_test_name}
         PROPERTIES
@@ -78,9 +39,43 @@ function(add_unit_test test_name sources_var libs_var)
     target_link_libraries(
         ${unit_test_name}
         ${prefixed_libs}
-        libgtest
-        libgmock
+        gtest
+        gmock_main
     )
 
     add_test(NAME ${unit_test_name} COMMAND ${PROJECT_SOURCE_DIR}/test/bin/${unit_test_name})
 endfunction(add_unit_test)
+
+function(_add_lib lib_name sources_var directory)
+    add_library(
+        ${lib_name}
+        ${${sources_var}}
+    )
+
+    target_include_directories(
+        ${lib_name}
+        PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+
+    set_target_properties(
+        ${lib_name}
+        PROPERTIES
+        ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/${directory}/lib
+    )
+endfunction(_add_lib)
+
+function(add_test_lib test_lib_name sources_var)
+    _add_lib(
+        ${test_lib_name}
+        ${sources_var}
+        test
+    )
+endfunction(add_test_lib)
+
+function(add_mock_lib mck_lib_name sources_var)
+    _add_lib(
+        ${mck_lib_name}
+        ${sources_var}
+        mck
+    )
+endfunction(add_mock_lib)
