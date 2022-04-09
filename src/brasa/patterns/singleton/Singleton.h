f4c66abf32df17191db408ef8bc41a0d2106c0ad
Marcelo Pinto <mpinto70@gmail.com>
2022-04-09 20:21:02 -0300

    Allow polymorphic singletons

--- src/brasa/patterns/singleton/Singleton.h
+++ src/brasa/patterns/singleton/Singleton.h
@@ -14,6 +14,10 @@ class Singleton final {
 public:
     template <typename... ARGS>
     static T& create_instance(ARGS&&... args);
+    template <typename U, typename... ARGS>
+    static T& create_instance(ARGS&&... args);
+    template <typename U>
+    static T& create_instance(std::unique_ptr<U> u);
     static T& instance();
     static void free_instance();
     static bool has_instance();
@@ -46,6 +50,30 @@ T& Singleton<T>::create_instance(ARGS&&... args) {
         throw std::logic_error("brasa::pattern::Singleton::create_instance already created for "s + typeid(T).name());
     }
     t_ = std::make_unique<T>(std::forward<ARGS>(args)...);
+    return *t_;
+}
+
+template <typename T>
+template <typename U, typename... ARGS>
+T& Singleton<T>::create_instance(ARGS&&... args) {
+    std::unique_lock lock(mutex_);
+    if (t_ != nullptr) {
+        using namespace std::string_literals;
+        throw std::logic_error("brasa::pattern::Singleton::create_instance already created for "s + typeid(T).name());
+    }
+    t_ = std::make_unique<U>(std::forward<ARGS>(args)...);
+    return *t_;
+}
+
+template <typename T>
+template <typename U>
+T& Singleton<T>::create_instance(std::unique_ptr<U> u) {
+    std::unique_lock lock(mutex_);
+    if (t_ != nullptr) {
+        using namespace std::string_literals;
+        throw std::logic_error("brasa::pattern::Singleton::create_instance already created for "s + typeid(T).name());
+    }
+    t_ = std::move(u);
     return *t_;
 }
 
