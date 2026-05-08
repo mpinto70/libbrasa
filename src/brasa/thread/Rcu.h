7c0340a4fb7ade6307846435a6d11dbc57a67d1b
Marcelo Pinto <mpinto70@gmail.com>
2026-05-07 23:11:13 -0400

    Improve thread

--- src/brasa/thread/Rcu.h
+++ src/brasa/thread/Rcu.h
@@ -4,64 +4,91 @@
 #include <brasa/thread/RcuWriter.h>
 
 #include <algorithm>
+#include <atomic>
 #include <list>
 #include <mutex>
 
 namespace brasa::thread {
 
-/** Class that implements a user space RCU (https://en.wikipedia.org/wiki/Read-copy-update) of
+/**
+ * Class that implements a user space RCU (https://en.wikipedia.org/wiki/Read-copy-update) of
  * a \b single value of type T. It is important to assure that the lifetime of this object is longer
  * than the lifetime of any `RcuReader` or `RcuWriter` object.
  */
 template <typename T>
 class Rcu {
 public:
-    /** Creates the RCU object with the initial value.
+    /**
+     * Creates the RCU object with the initial value.
+     *
      * @param t the initial value.
      */
     explicit Rcu(T t);
-    /** Return the number of available objects. */
+    /**
+     * Return the number of available objects.
+     *
+     * @return the number of nodes currently in the list.
+     * @note Must not be called while a `RcuWriter` is active (i.e., between `write()` and
+     *       the destruction of the returned `RcuWriter`), as `nodes_mutex_` is held during
+     *       that window and `size()` would deadlock.
+     */
     size_t size() const noexcept { return nodes_.size(); }
-    /** Get a read-only reference to the value.
+    /**
+     * Get a read-only reference to the value.
      * Any number of readers can call this function and the contention is minimal: current_mutex_ is
      * locked to assure proper reference counting. When the `RcuReader` object is destroyed,
-     * `release()` is called and `curent_mutex_` is locked again to assure proper reference
+     * `release()` is called and `current_mutex_` is locked again to assure proper reference
      * counting.
+     *
+     * @return an `RcuReader` holding a read-only reference to the current value.
      */
     RcuReader<T, Rcu<T>> read() const noexcept;
-    /** Get a read/write reference to change current value.
+    /**
+     * Get a read/write reference to change current value.
      * This function locks nodes_mutex_ and will block all other calls to `write()` until the
-     * returned `RcuWriter` object is destroyed, and `update()` is called, releasing the lock on
-     * `nodes_mutex_`.
+     * returned `RcuWriter` object is destroyed and `update()` is called, releasing the lock on
+     * `nodes_mutex_`. The caller must ensure that the `RcuWriter` object is eventually destroyed
+     * so that `update()` is called and `nodes_mutex_` is released.
+     *
+     * @return an `RcuWriter` holding a read/write reference to a copy of the current value.
      */
     RcuWriter<T, Rcu<T>> write();
 
 private:
     /** The values are stored in this ref-counted structure. */
     struct Node {
-        /** Creates a new node with the initial value, and ref-count set to 0
+        /**
+         * Creates a new node with the initial value, and ref-count set to 0.
+         *
          * @param value the initial value.
          */
         explicit Node(T value) noexcept : refcount_(0), value_(std::move(value)) {}
-        mutable int refcount_; ///< number of readers.
-        T value_;              ///< the value.
+        mutable std::atomic<int> refcount_; ///< number of readers.
+        T value_;                           ///< the value.
     };
 
-    std::mutex nodes_mutex_;           ///< mutex to protect the nodes list (write mutex).
+    mutable std::mutex nodes_mutex_;   ///< mutex to protect the nodes list (write mutex).
     std::list<Node> nodes_;            ///< list of nodes with different versions of the value.
     mutable std::mutex current_mutex_; ///< mutex to protect the current node (read mutex).
     const Node* current_;              ///< pointer to the current node (the last one in the list).
 
-    /** Function that is called when the `RcuReader` object is destroyed.
+    /**
+     * Function that is called when the `RcuReader` object is destroyed.
+     *
      * @param t the pointer to the value so it can be found to decrease the ref-count.
      */
     void release(const T* t) const noexcept;
     friend class RcuReader<T, Rcu<T>>; ///< It is a friend so it can call `release()`.
 
-    /** Function that is called when the `RcuWriter` object is destroyed.
+    /**
+     * Function that is called when the `RcuWriter` object is destroyed.
      * It also performs the cleanup of the nodes that are not being used anymore.
      * Note that this cleanup has to lock `current_mutex_` once to set `current_` to point to the
-     * new value and repeatedly while attempting to remove unused values.
+     * new value. Then it removes unused nodes (refcount == 0). Safe traversal without additional
+     * locking because `std::list` erasure does not invalidate other iterators, and any node still
+     * referenced has a positive refcount so it won't be erased concurrently.
+     *
+     * @note `nodes_mutex_` is acquired in `write()` and released at the end of this function.
      */
     void update() noexcept;
     friend class RcuWriter<T, Rcu<T>>; ///< It is a friend so it can call `update()`.
@@ -90,8 +117,7 @@ void Rcu<T>::release(const T* t) const noexcept {
         return &node.value_ == t;
     });
     if (it != nodes_.end()) {
-        std::lock_guard lock(current_mutex_);
-        it->refcount_ -= 1;
+        it->refcount_.fetch_sub(1);
     }
 }
 
@@ -100,7 +126,7 @@ RcuWriter<T, Rcu<T>> Rcu<T>::write() {
     nodes_mutex_.lock();
 
     try {
-        nodes_.push_back(nodes_.back());
+        nodes_.emplace_back(nodes_.back().value_);
         return RcuWriter<T, Rcu<T>>(&nodes_.back().value_, this);
     } catch (...) {
         nodes_mutex_.unlock();
@@ -117,16 +143,8 @@ void Rcu<T>::update() noexcept {
 
     // remove unreferenced nodes that are not the last one
     for (auto it = nodes_.begin(); std::next(it) != nodes_.end();) {
-        // check if refcount is zero before locking
-        if (it->refcount_ == 0) {
-            // now that we know refcount is zero, lock and check again
-            std::lock_guard lock(current_mutex_);
-            if (it->refcount_ == 0) {
-                it = nodes_.erase(it);
-                continue;
-            } else {
-                ++it;
-            }
+        if (it->refcount_.load() == 0) {
+            it = nodes_.erase(it);
         } else {
             ++it;
         }
--- src/brasa/thread/RcuWriter.h
+++ src/brasa/thread/RcuWriter.h
@@ -2,16 +2,19 @@
 
 namespace brasa::thread {
 
-/** Class responsible to hold the \b read/write reference to the copy of the current value.
+/**
+ * Class responsible to hold the \b read/write reference to the copy of the current value.
  * After destruction, the value of this class will be the new current value.
  */
 template <typename T, typename POOL>
 class RcuWriter {
 public:
-    /** Creates the RcuWriter object with the ref to the new current value.
-     * @param t the pointer to the new current value.
+    /**
+     * Creates the RcuWriter object with the ref to the new current value.
+     *
+     * @param t    the pointer to the new current value.
      * @param pool the pointer to the pool that is the owner of the value and will receive it back
-     * via update.
+     *             via update.
      */
     RcuWriter(T* t, POOL* pool) noexcept : value_(t), pool_(pool) {}
     ~RcuWriter() noexcept;
@@ -20,9 +23,19 @@ public:
     RcuWriter& operator=(const RcuWriter&) = delete;
     RcuWriter& operator=(RcuWriter&&);
 
-    /** Return the read/write reference to the value stored in the pool. */
+    /**
+     * Return the read/write reference to the value stored in the pool.
+     *
+     * @return the read/write reference to the value.
+     * @warning Calling this on an invalid (moved-from) writer is undefined behavior.
+     *          Use `is_valid()` to check first.
+     */
     T& value() const noexcept { return *value_; }
-    /** Return if the object has a valid value. */
+    /**
+     * Returns whether the object holds a valid value.
+     *
+     * @return true if the object holds a valid value, false otherwise.
+     */
     bool is_valid() const noexcept { return value_ != nullptr && pool_ != nullptr; }
 
 private:
@@ -50,6 +63,9 @@ RcuWriter<T, POOL>::RcuWriter(RcuWriter&& other) : value_(other.value_),
 
 template <typename T, typename POOL>
 RcuWriter<T, POOL>& RcuWriter<T, POOL>::operator=(RcuWriter&& other) {
+    if (value_ != nullptr && pool_ != nullptr) {
+        pool_->update();
+    }
     value_ = other.value_;
     pool_ = other.pool_;
     other.value_ = nullptr;
